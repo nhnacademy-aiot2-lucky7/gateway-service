04b82fa19184f872c2e2a3fd0eef27df7acd5dbe
{영찬} <thddudcks12@naver.com>
2025-05-13 09:29:35 +0900

    수정 - 더미 데이터 생성

--- src/main/java/com/nhnacademy/gateway/mqtt/common/MqttConfig.java
+++ src/main/java/com/nhnacademy/gateway/mqtt/common/MqttConfig.java
@@ -3,20 +3,19 @@ package com.nhnacademy.gateway.mqtt.common;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MqttConfig {
 
-    private static final String BROKER = "tcp://172.19.0.3:1883"; // 로컬 환경 테스트 용도
-
-//    private static final String BROKER = "tcp://localhost:10240";
+    private static final String BROKER = "tcp://localhost:10240";
     private static final String CLIENT_ID = "spring-mqtt-client";
 
     @Bean
     public MqttClient mqttClient() throws MqttException {
-        MqttClient client = new MqttClient(BROKER, CLIENT_ID);
+        MqttClient client = new MqttClient(BROKER, CLIENT_ID, new MemoryPersistence());
         MqttConnectOptions options = new MqttConnectOptions();
         options.setAutomaticReconnect(true);
         options.setCleanSession(true);
