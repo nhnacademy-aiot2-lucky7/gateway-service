8b3423b9b6f92cd22ad00afb66707cd1e6e19e47
nink2458 <nink2458@gmail.com>
2025-06-06 19:15:11 +0900

    긴급 추가: `Gateway` 영역에 Web Front 요구사항인 게이트웨이 상세 정보 페이지에 사용될 데이터 조회 기능을 추가

---
+++ src/main/java/com/nhnacademy/gateway/infrastructure/adapter/SensorDataServiceAdapter.java
@@ -0,0 +1,18 @@
+package com.nhnacademy.gateway.infrastructure.adapter;
+
+import com.nhnacademy.gateway.infrastructure.dto.SensorDataDetailResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "sensor-service")
+public interface SensorDataServiceAdapter {
+
+    @GetMapping("/sensor-data-mappings/gateway-id/{gateway-id}/sensors")
+    ResponseEntity<List<SensorDataDetailResponse>> getSensorDataDetailsByGatewayId(
+            @PathVariable("gateway-id") Long gatewayId
+    );
+}
